c58006323fb3bf8e1ad46687e6b6741f73e8c300
jdegaris <j.a.degaris@gmail.com>
2020-02-23 14:45:45 -0500

    connected DB and added CRUD functionality to books

--- controllers/books.js
+++ controllers/books.js
@@ -1,45 +1,106 @@
+const Book = require('../models/Book')
+
 // @desc    Get all books
 // @route   GET /api/v1/books
 // @access  Public
-exports.getBooks = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: 'Show all books'
-    })
+exports.getBooks = async (req, res, next) => {
+    try {
+        const books = await Book.find()
+        res.status(200).json({
+            success: true,
+            count: books.length,
+            data: books
+        })
+    } catch (err) {
+        res.status(400).json({
+            success: false
+        })
+    }
+
 }
 // @desc    Get single book
 // @route   GET /api/v1/books/:id
 // @access  Public
-exports.getBook = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `Show book with id ${req.params.id}`
-    })
+exports.getBook = async (req, res, next) => {
+    try {
+        const book = await Book.findById(req.params.id)
+
+        if (!book) {
+            return res.status(400).json({
+                success: false
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: book
+        })
+    } catch (err) {
+        res.status(400).json({
+            success: false
+        })
+    }
 }
 // @desc    Create new book
 // @route   POST /api/v1/books
 // @access  Private
-exports.createBook = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: 'Create a new book'
-    })
+exports.createBook = async (req, res, next) => {
+    try {
+        const book = await Book.create(req.body)
+        res.status(201).json({
+            success: true,
+            data: book
+        })
+    } catch (err) {
+        res.status(400).json({
+            success: false
+        })
+    }
+
 }
 // @desc    Update a book
 // @route   PUT /api/v1/books
 // @access  Private
-exports.updateBook = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `Update book with id ${req.params.id}`
-    })
+exports.updateBook = async (req, res, next) => {
+    try {
+        const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if (!book) {
+            return res.status(400).json({
+                success: false
+            })
+        }
+        res.status(200).json({
+            success: true,
+            data: book
+        })
+    } catch (err) {
+        res.status(400).json({
+            success: false
+        })
+    }
+
 }
 // @desc    Delete a book
 // @route   DELETE /api/v1/books
 // @access  Private
-exports.deleteBook = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `Delete book with id ${req.params.id}`
-    })
+exports.deleteBook = async (req, res, next) => {
+    try {
+        const book = await Book.findByIdAndDelete(req.params.id)
+        if (!book) {
+            return res.status(400).json({
+                success: false
+            })
+        }
+        res.status(200).json({
+            success: true,
+            data: {}
+        })
+    } catch (err) {
+        res.status(400).json({
+            success: false
+        })
+    }
 }
--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-// const colors = require('colors');
+const colors = require('colors');
 // const errorHandler = require('./middleware/error')
-// const connectDB = require('./config/db')
-
-// Route files
-const books = require('./routes/books')
+const connectDB = require('./config/db')
 
 // Load env vars
 dotenv.config({
   path: './config/config.env'
 });
 
+// Connect to database
+connectDB()
+
+// Route files
+const books = require('./routes/books')
+
 const app = express();
+
+// Body parser
+app.use(express.json())
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -28,5 +34,12 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  // Close server and exit process
+  server.close(() => process.exit(1))
+})
